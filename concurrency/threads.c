c8035fc9c9fb77d37338af261f843302ae1442ad
Ato Aidoo <atoaidook@gmail.com>
2023-12-04 22:57:46 -0500

    Counter Race Condition

--- concurrency/threads.c
+++ concurrency/threads.c
@@ -2,15 +2,11 @@
 #include <assert.h>
 #include <pthread.h>
 
-
-
 void *mythread(void *arg){
     //cast arg to a char pointer
     printf("%s\n", (char *) arg);
     return NULL;
 }
-
-
 
 int main(int argc, char *argv[]){
 
@@ -19,10 +15,8 @@ int main(int argc, char *argv[]){
 
     printf("main begin\n");
 
-
     rc = pthread_create(&p1, NULL, mythread, "A"); assert(rc==0);
     rc = pthread_create(&p2, NULL, mythread, "B"); assert(rc==0);
-
 
     rc = pthread_join(p1, NULL); assert(rc==0);
     rc = pthread_join(p2, NULL); assert(rc==0);
